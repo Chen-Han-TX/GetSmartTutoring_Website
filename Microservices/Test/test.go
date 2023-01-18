8d9e8b02189cd844ca484c7eafabcfd6f8bbe0c4
Chen Han <73086331+Chen-Han-NP@users.noreply.github.com>
2023-01-18 22:06:09 +0800

    tested get data and delete data for realtime database operation on Test.go

--- Microservices/Test/test.go
+++ Microservices/Test/test.go
@@ -9,6 +9,10 @@ import (
 	"google.golang.org/api/option"
 )
 
+type UserScore struct {
+	Score int `json:"score"`
+}
+
 func main() {
 	ctx := context.Background()
 
@@ -30,6 +34,8 @@ func main() {
 		log.Fatalln("error in creating firebase DB client: ", err)
 	}
 
+	// -------New/Update Data--------
+
 	// create ref at path user_scores/:userId
 	ref := client.NewRef("user_scores/" + fmt.Sprint(1))
 
@@ -38,5 +44,26 @@ func main() {
 	}
 
 	fmt.Println("score added/updated successfully!")
+
+	// -------Retreiving------
+	// get database reference to user score
+	ref2 := client.NewRef("user_scores/1")
+
+	// read from user_scores using ref
+	var s UserScore
+	if err := ref2.Get(context.TODO(), &s); err != nil {
+		log.Fatalln("error in reading from firebase DB: ", err)
+	}
+	fmt.Println("retrieved user's score is: ", s.Score)
+
+	// ----DELETE-----
+	/*
+		ref3 := client.NewRef("user_scores/1")
+
+		if err := ref3.Delete(context.TODO()); err != nil {
+			log.Fatalln("error in deleting ref: ", err)
+		}
+		fmt.Println("user's score deleted successfully:)")
+	*/
 
 }
